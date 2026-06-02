aa0a958822b3870d6a1b3c5ab3fdf13b45974fb3
Shiyue-Su <15002442161@163.com>
2026-06-02 15:29:58 +0800

    Update exp_basic.py

--- exp/exp_basic.py
+++ exp/exp_basic.py
@@ -1,10 +1,18 @@
 import os
 import torch
-from models import eeg_basis_mixer_v2_nobasis, eeg_basis_mixer_v2_nobasis_onlyT, eeg_basis_mixer_v2_nobasis_noT, eeg_basis_mixer_v2_nobasis_layernorm, eeg_basis_mixer_v2_nobasis_mlla, eeg_mixer_v3_tconv, eeg_mixer_v4_outAttentionPoolingFusion
+from models import (
+    eeg_basis_mixer_v2_nobasis,
+    eeg_basis_mixer_v2_nobasis_layernorm,
+    eeg_basis_mixer_v2_nobasis_mlla,
+    eeg_basis_mixer_v2_nobasis_noT,
+    eeg_basis_mixer_v2_nobasis_onlyT,
+    eeg_mixer_v3_tconv,
+    eeg_mixer_v4_outAttentionPoolingFusion,
+    eeg_mixer_v11_1_spatial_multilevel,
+)
 class Exp_Basic(object):
     def __init__(self, args):
         self.args = args
-        # Only the nobasis baseline is registered in this release.
         self.model_dict = {
             "eeg_basis_mixer_v2_nobasis": eeg_basis_mixer_v2_nobasis,
             "eeg_basis_mixer_v2_nobasis_onlyT": eeg_basis_mixer_v2_nobasis_onlyT,
@@ -12,7 +20,8 @@ class Exp_Basic(object):
             "eeg_basis_mixer_v2_nobasis_layernorm": eeg_basis_mixer_v2_nobasis_layernorm,
             "eeg_basis_mixer_v2_nobasis_mlla": eeg_basis_mixer_v2_nobasis_mlla,
             "eeg_mixer_v3_tconv": eeg_mixer_v3_tconv,
-            "eeg_mixer_v4_outAttentionPoolingFusion": eeg_mixer_v4_outAttentionPoolingFusion
+            "eeg_mixer_v4_outAttentionPoolingFusion": eeg_mixer_v4_outAttentionPoolingFusion,
+            "eeg_mixer_v11_1_spatial_multilevel": eeg_mixer_v11_1_spatial_multilevel
         }
         self.device = self._acquire_device()
         self.model = self._build_model().to(self.device)
@@ -24,13 +33,17 @@ class Exp_Basic(object):
     def _acquire_device(self):
         if self.args.use_gpu:
             if self.args.use_multi_gpu:
-                os.environ["CUDA_VISIBLE_DEVICES"] = self.args.devices
+                if not os.environ.get("CUDA_VISIBLE_DEVICES"):
+                    os.environ["CUDA_VISIBLE_DEVICES"] = self.args.devices
+                visible = os.environ.get("CUDA_VISIBLE_DEVICES", self.args.devices)
                 device = torch.device("cuda:0")
-                print("Use GPU (multi): visible={} -> cuda:0".format(self.args.devices))
+                print("Use GPU (multi): visible={} -> cuda:0".format(visible))
             else:
-                os.environ["CUDA_VISIBLE_DEVICES"] = str(self.args.gpu)
+                if not os.environ.get("CUDA_VISIBLE_DEVICES"):
+                    os.environ["CUDA_VISIBLE_DEVICES"] = str(self.args.gpu)
+                visible = os.environ.get("CUDA_VISIBLE_DEVICES", str(self.args.gpu))
                 device = torch.device("cuda:0")
-                print("Use GPU (single): visible={} -> cuda:0".format(self.args.gpu))
+                print("Use GPU (single): visible={} -> cuda:0".format(visible))
         else:
             device = torch.device("cpu")
             print("Use CPU")
